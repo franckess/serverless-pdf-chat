7f43a261bed2574d9597826fdb4dd016948b5332
franckess <franckess48@gmail.com>
2024-06-08 00:27:04 +1000

    First commit

--- backend/src/generate_response/main.py
+++ backend/src/generate_response/main.py
@@ -4,44 +4,49 @@ from aws_lambda_powertools import Logger
 from langchain.llms.bedrock import Bedrock
 from langchain.memory.chat_message_histories import DynamoDBChatMessageHistory
 from langchain.memory import ConversationBufferMemory
-from langchain.embeddings import BedrockEmbeddings
-from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
 
 
 MEMORY_TABLE = os.environ["MEMORY_TABLE"]
-BUCKET = os.environ["BUCKET"]
+KNOWLEDGE_BASE_DETAILS_SSM_PATH = os.environ["KNOWLEDGE_BASE_DETAILS_SSM_PATH"]
 
 
 s3 = boto3.client("s3")
+ssm = boto3.client("ssm")
 logger = Logger()
 
 
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event, context):
     event_body = json.loads(event["body"])
-    file_name = event_body["fileName"]
     human_input = event_body["prompt"]
     conversation_id = event["pathParameters"]["conversationid"]
+    param_response = ssm.get_parameter(Name=KNOWLEDGE_BASE_DETAILS_SSM_PATH)
+    knowledge_base_details = json.loads(param_response['Parameter']['Value'])
+    user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
 
-    user = event["requestContext"]["authorizer"]["claims"]["sub"]
-
-    s3.download_file(BUCKET, f"{user}/{file_name}/index.faiss", "/tmp/index.faiss")
-    s3.download_file(BUCKET, f"{user}/{file_name}/index.pkl", "/tmp/index.pkl")
+    # s3.download_file(BUCKET, f"{user}/{file_name}/index.faiss", "/tmp/index.faiss")
+    # s3.download_file(BUCKET, f"{user}/{file_name}/index.pkl", "/tmp/index.pkl")
 
     bedrock_runtime = boto3.client(
         service_name="bedrock-runtime",
-        region_name="us-east-1",
+        region_name="ap-southeast-2",
     )
 
-    embeddings, llm = BedrockEmbeddings(
-        model_id="amazon.titan-embed-text-v1",
-        client=bedrock_runtime,
-        region_name="us-east-1",
-    ), Bedrock(
-        model_id="anthropic.claude-v2", client=bedrock_runtime, region_name="us-east-1"
+    llm = Bedrock(
+        model_id="mistral.mistral-7b-instruct-v0:2", client=bedrock_runtime, region_name="ap-southeast-2", streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()]
     )
-    faiss_index = FAISS.load_local("/tmp", embeddings)
+
+    retriever = AmazonKnowledgeBasesRetriever(
+        knowledge_base_id=knowledge_base_details["knowledgeBaseId"],
+        retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 4, 
+                                                        "equals": {"key": "userid", "value": user_id}
+                                                       }
+                         },
+    )
 
     message_history = DynamoDBChatMessageHistory(
         table_name=MEMORY_TABLE, session_id=conversation_id
@@ -57,22 +62,26 @@ def lambda_handler(event, context):
 
     qa = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        retriever=faiss_index.as_retriever(),
+        retriever=retriever,
         memory=memory,
-        return_source_documents=True,
+        return_source_documents=True
     )
 
-    res = qa({"question": human_input})
+    try:
+        res = qa({"question": human_input})
+        logger.info(f'response from llm: {res}')
 
-    logger.info(res)
+        return {
+            "statusCode": 200,
+            "headers": {
+               "Content-Type": "application/json",
+               "Access-Control-Allow-Headers": "*",
+               "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*",
+            },
+            "body": json.dumps(res["answer"]),
+        }
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "*",
-        },
-        "body": json.dumps(res["answer"]),
-    }
+    except Exception as e:
+       logger.error(f'Exception: {e}')
+       raise e
--- backend/src/upload_trigger/main.py
+++ backend/src/upload_trigger/main.py
@@ -8,17 +8,29 @@ from aws_lambda_powertools import Logger
 
 DOCUMENT_TABLE = os.environ["DOCUMENT_TABLE"]
 MEMORY_TABLE = os.environ["MEMORY_TABLE"]
-QUEUE = os.environ["QUEUE"]
 BUCKET = os.environ["BUCKET"]
+KNOWLEDGE_BASE_DETAILS_SSM_PATH = os.environ["KNOWLEDGE_BASE_DETAILS_SSM_PATH"]
 
 
 ddb = boto3.resource("dynamodb")
+bedrock = boto3.client("bedrock-agent")
 document_table = ddb.Table(DOCUMENT_TABLE)
 memory_table = ddb.Table(MEMORY_TABLE)
-sqs = boto3.client("sqs")
+ssm = boto3.client("ssm")
+
 s3 = boto3.client("s3")
 logger = Logger()
 
+def fix_json(json_string):
+    # Replace single quotes with double quotes and escaping internal quotes if needed
+    fixed_json = json_string.replace("'", '"')
+    try:
+        # Try loading the JSON to check if it's valid
+        data = json.loads(fixed_json)
+        return data
+    except json.JSONDecodeError as e:
+        logger.error(f"Failed to decode JSON: {e}")
+        return None
 
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event, context):
@@ -59,9 +71,36 @@ def lambda_handler(event, context):
     conversation = {"SessionId": conversation_id, "History": []}
     memory_table.put_item(Item=conversation)
 
-    message = {
-        "documentid": document_id,
-        "key": key,
-        "user": user_id,
+    param_response = ssm.get_parameter(Name=KNOWLEDGE_BASE_DETAILS_SSM_PATH)
+    print('***********************************')
+    pprint.pp(param_response['Parameter']['Value'], depth=1)
+
+    # Use the fix_json function to attempt to correct and parse the JSON
+    knowledge_base_details = fix_json(param_response['Parameter']['Value'])
+    if knowledge_base_details:
+        try:
+            bedrock.start_ingestion_job(
+                knowledgeBaseId=knowledge_base_details['knowledgeBaseId'],
+                dataSourceId=knowledge_base_details['dataSourceId']
+            )
+        except Exception as e:
+            logger.error(f'Error triggering bedrock knowledge base sync: {e}')
+    else:
+        logger.error("Failed to fix and decode JSON from SSM parameter.")
+
+    # Create metadata file
+    metadata = {
+        "metadataAttributes": {
+            "userid": user_id,
+            "documentid": document_id,
+            "filename": file_name,
+            "created": timestamp_str,
+            "pages": pages
+        }
     }
-    sqs.send_message(QueueUrl=QUEUE, MessageBody=json.dumps(message))
+    metadata_file_path = f"/tmp/{file_name}.metadata.json"
+    with open(metadata_file_path, 'w') as metadata_file:
+        json.dump(metadata, metadata_file)
+
+    # Upload metadata file to S3
+    s3.upload_file(metadata_file_path, BUCKET, key)
